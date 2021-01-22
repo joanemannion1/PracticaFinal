cf04fd0f8eb0cb283076b6edfac76842a9f07a0e
Joane Mannion <alu0101451701@ull.edu.es>
2021-01-22 12:28:04 +0000

    Creando pruebas de la clase Cultivo

---
+++ spec/cultivo_spec.rb
@@ -0,0 +1,40 @@
+require_relative "../lib/cultivos/cultivo"
+
+RSpec.describe Cultivos do
+	context Cultivo do
+		before (:each) do
+			@c1 =  Cultivo.new('Cultivo1', 0.3, 12, 0.7, 0.5)
+		end
+		context "Creacion de un cultivo" do
+			it "Instanciar un cultivo" do
+				expect(@c1).not_to eq(nil)
+			end
+			
+			it "Obtener nombre de un cultivo" do
+				expect(@c1.nombre).to eq("Cultivo1")
+			end
+
+			it "Se obtiene una cadena con la informacion del cultivo" do
+				expect(@c1.to_s).to eq("Nombre: Cultivo1, Emision de gases: 0.3, Terreno: 12, Precio: 0.7, Coste: 0.5)
+			end
+			
+			it "Obtener las emisiones de gases de efecto invernadero" do
+				expect(@c1.g).to eq(0.3)
+			end
+
+			it "Obtener el terreno utilizado" do
+                                expect(@c1.t).to eq(12)
+                        end
+
+			it "Obtener el precio de venta" do
+                                expect(@c1.p).to eq(0.7)
+                        end
+
+			it "Obtener el coste de produccion" do
+                                expect(@c1.c).to eq(0.5)
+                        end
+		end
+	end
+end
+
+
