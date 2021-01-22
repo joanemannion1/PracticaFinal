f2c5b517c7a41fe0e9483e58eab5b21a4ebb2153
Joane Mannion <alu0101451701@ull.edu.es>
2021-01-22 13:05:49 +0000

    Clase Cereal creada con pruebas satisfactorias

---
+++ lib/cultivos/cereal.rb
@@ -0,0 +1,42 @@
+require_relative "cultivo"
+
+class Cereal < Cultivo
+	include Comparable
+	attr_reader :tipo, :estacion, :densidad, :planta, :npmc, :pmg, :pg, :pu, :gl, :pml, :ds
+
+	def initialize(n,g,t,p,c,tip,e, npmc, pmg, pg, pu, gl, pml, ds)
+		super(n,g,t,p,c)
+		@tipo = tip
+		@estacion = e
+		@npmc = npmc
+		@pmg = pmg
+		@pg = pg
+		@pu = pu
+		@gl = gl
+		@pml = pml
+		@ds = ds
+	end
+	
+	def to_s()
+		s = "Cereal ->"
+		s << super.to_s
+		s << ", tipo: #{@tipo}, estacion: #{@estacion}"
+		s
+	end
+
+	def densidad()
+		return (@npmc * (@pmg / 100.0))/ (@pg * @pu * @gl)
+
+	end
+
+	def planta()
+		return ((@pml * 100.0) / @ds)
+	end
+
+	def <=> other 
+		[@precio,@coste] <=> [other.precio, other.coste]
+	end
+end
+
+
+		
---
+++ lib/cultivos/cultivo.rb
@@ -0,0 +1,16 @@
+class Cultivo
+	attr_reader :nombre, :gas, :terreno, :precio, :coste
+
+	def initialize(n,g,t,p,c)
+		@nombre = n
+		@gas = g
+		@terreno = t
+		@precio = p
+		@coste = c
+	end
+	
+	def to_s()
+		"Nombre: #{@nombre}, Emision de gases: #{@gas}, Terreno: #{@terreno}, Precio: #{@precio}, Coste: #{@coste}"
+	end
+
+end
